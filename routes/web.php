0f004993d1722aac09fa2e8da6131e6107863903
Jairo Lopez <lopcast266@gmail.com>
2024-07-14 15:22:29 -0600

    Se agregaron 3 controladores - [Prestamo,Reserva,Devolver]

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/',  [LibroController::class, 'show_all_book'])->name('show_all_book
 Route::post('/guardar-libro',  [LibroController::class, 'guardarLibro'])->name('guardar_libro');
 Route::post('/actualizar-libro',  [LibroController::class, 'actualizarLibro'])->name('actualizar_libro');
 Route::delete('/eliminar-libro',  [LibroController::class, 'eliminarLibro'])->name('eliminar_libro');
+//Rutas para logica de prestamos y reservas
+Route::post('/reserva',[ReservaController::class,'resevarLibro'])->name('reservar_libro');
+Route::post('/prestamo',[PrestamoController::class,'prestamoLibro'])->name('prestamo_libro');
+Route::post('/devolver',[DevolverController::class,'devolverLibro'])->name('devolver_libro');
